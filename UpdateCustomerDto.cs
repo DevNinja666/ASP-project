af86f94dcf1fb6619dcc493351c6227db01224e2
Elnur <elnurgasanovv07@gmail.com>
2026-03-02 14:35:28 +0400

    Add UpdateCustomerDto class for customer updates

---
+++ UpdateCustomerDto.cs
@@ -0,0 +1,9 @@
+namespace InvoiceManager.Api.DTOs;
+
+public class UpdateCustomerDto
+{
+    public string Name { get; set; } = null!;
+    public string? Address { get; set; }
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+}
