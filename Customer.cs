d45a31270952b52d77fd0691b2b6986646965c04
Elnur <elnurgasanovv07@gmail.com>
2026-03-02 14:30:06 +0400

    Create Customer.cs

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+namespace InvoiceManager.Api.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Address { get; set; }
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+
+    public ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
+}
