0cfd190d291190e8a4f22dbba5407a8a5810d4ae
Elnur <elnurgasanovv07@gmail.com>
2026-03-03 23:13:35 +0400

    Create RegisterUserDto.cs

---
+++ dto/RegisterUserDto.cs
@@ -0,0 +1,8 @@
+public class RegisterUserDto
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+}
