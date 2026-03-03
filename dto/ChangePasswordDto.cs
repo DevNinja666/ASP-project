18131c9686982adc105b926bec89e9fb010dcefb
Elnur <elnurgasanovv07@gmail.com>
2026-03-03 23:17:04 +0400

    Create ChangePasswordDto.cs

---
+++ dto/ChangePasswordDto.cs
@@ -0,0 +1,12 @@
+namespace Invoicer.DTOs.User
+{
+
+    public class ChangePasswordDto
+    {
+      
+        public string CurrentPassword { get; set; } = null!;
+
+     
+        public string NewPassword { get; set; } = null!;
+    }
+}
