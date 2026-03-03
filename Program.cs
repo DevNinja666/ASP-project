2e699695183a069a91da248d09c418aff4877ac4
Elnur <elnurgasanovv07@gmail.com>
2026-03-04 00:25:21 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,21 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using InvoiceManager.Api.Data;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection;
+using Invoicer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -28,6 +23,8 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
